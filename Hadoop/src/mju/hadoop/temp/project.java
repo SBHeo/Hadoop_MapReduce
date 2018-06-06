80c594ecbc264c019057568d6e4ca1553e706ecc
SBHeo <tmdqja7931@gmail.com>
2018-06-06 00:29:38 -0700

    %

--- Hadoop/src/mju/hadoop/temp/project.java
+++ Hadoop/src/mju/hadoop/temp/project.java
@@ -1,9 +1,14 @@
 package mju.hadoop.temp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -21,21 +26,23 @@ public class project {
 	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 		private Text keyData = new Text();
 		private IntWritable valueData = new IntWritable(1);
+		private Text total = new Text("total");
 
 		@Override
 		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
 			String[] values = line.split(","); // 1 - time , 7 - color
-			
+
 			String[] time = values[1].split(":", 7); // 0 - h, 1 - m, 2 - s
 			int hour = Integer.parseInt(time[0]);
 			if (hour >= 20 || hour < 6) {
 				if (!values[7].equals("0")) {
 					keyData.set(values[7]);
 					context.write(keyData, valueData);
+					context.write(total, valueData);
 				}
 			}
-			
+
 		}
 	}
 
@@ -43,7 +50,8 @@ public class project {
 		private IntWritable value = new IntWritable();
 
 		@Override
-		protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
 			int sum = 0;
 			for (IntWritable value : values)
 				sum += value.get();
@@ -51,7 +59,7 @@ public class project {
 			context.write(key, value);
 		}
 	}
-	
+
 	public static class Map2 extends Mapper<LongWritable, Text, IntWritable, Text> {
 		private IntWritable key2 = new IntWritable();
 		private Text value2 = new Text();
@@ -60,18 +68,20 @@ public class project {
 		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
 			String[] values = line.split("\t");
-			
-			key2.set(Integer.parseInt(values[1]));
-			value2.set(values[0]);
-			
-			context.write(key2, value2);
+
+			if (!values[0].equals("total")) {
+				key2.set(Integer.parseInt(values[1]));
+				value2.set(values[0]);
+
+				context.write(key2, value2);
+			}
 		}
 	}
-	
+
 	public static class Compare2 extends WritableComparator {
-		
-		protected Compare2(){
-			super(IntWritable.class,true);
+
+		protected Compare2() {
+			super(IntWritable.class, true);
 		}
 
 		@SuppressWarnings("rawtypes")
@@ -79,22 +89,30 @@ public class project {
 		public int compare(WritableComparable o1, WritableComparable o2) {
 			IntWritable k1 = (IntWritable) o1;
 			IntWritable k2 = (IntWritable) o2;
-			
+
 			int cmp = k1.compareTo(k2);
-			
+
 			return -1 * cmp;
-		}	
+		}
 	}
 
-	public static class Reduce2 extends Reducer<IntWritable, Text, Text, IntWritable> {
+	public static class Reduce2 extends Reducer<IntWritable, Text, Text, DoubleWritable> {
 		private Text key2 = new Text();
+		private DoubleWritable value2 = new DoubleWritable();
 
 		@Override
-		protected void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			for(Text value : values){
+		protected void reduce(IntWritable key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+			Configuration conf = context.getConfiguration();
+			double total = Double.parseDouble(conf.get("total"));
+			for (Text value : values) {
 				key2.set(value);
 			}
-			context.write(key2, key);
+			int num = key.get();
+			double result = num / total * 100;
+			double result2 = Math.round(result*100d)/100d;
+			value2.set(result2);
+			context.write(key2, value2);
 		}
 	}
 
@@ -115,25 +133,38 @@ public class project {
 
 		boolean success = job.waitForCompletion(true);
 		System.out.println(success);
-		
-		String intput = args[1]+"/part-r-00000";
-		String output = args[1]+"Sort";
+
+		String uri = "/user/hadoop/" + args[1] + "/part-r-00000";
+		Configuration file = new Configuration();
+		FileSystem fs = FileSystem.get(file);
+		InputStream is = fs.open(new Path(uri));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			String[] tmp = line.split("\t");
+			if (tmp[0].equals("total")) {
+				conf.set("total", tmp[1]);
+			}
+		}
+
+		String input = args[1] + "/part-r-00000";
+		String output = args[1] + "Sort";
 		Job job2 = new Job(conf, "sort");
 		job2.setJarByClass(project.class);
 		job2.setMapOutputKeyClass(IntWritable.class);
 		job2.setMapOutputValueClass(Text.class);
 		job2.setOutputKeyClass(Text.class);
-		job2.setOutputValueClass(IntWritable.class);
+		job2.setOutputValueClass(DoubleWritable.class);
 		job2.setMapperClass(Map2.class);
 		job2.setReducerClass(Reduce2.class);
 		job2.setSortComparatorClass(Compare2.class);
 		job2.setInputFormatClass(TextInputFormat.class);
 		job2.setOutputFormatClass(TextOutputFormat.class);
 		job2.setNumReduceTasks(1);
-		
-		FileInputFormat.setInputPaths(job2, new Path(intput));
+
+		FileInputFormat.setInputPaths(job2, new Path(input));
 		FileOutputFormat.setOutputPath(job2, new Path(output));
-		
+
 		success = job2.waitForCompletion(true);
 		System.out.println(success);
 	}
